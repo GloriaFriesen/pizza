ce6545aef2701f840e7f89bdbe3c6d9bdc51cdea
Gloria Friesen <gloriafriesen@gmail.com>
2017-03-03 14:08:27 -0800

    add html and DOM manipulation in js to show details of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,25 @@ function Pizza (price, size, pizzaToppings) {
 }
 
 Pizza.prototype.changePriceSize = function() {
-  if (this.size === "small") {
+  if (this.size === "Small") {
     this.price -= 2
-  } else if (this.size === "large") {
+  } else if (this.size === "Large") {
   	this.price += 2
-  } else if (this.size === "extra-large") {
+  } else if (this.size === "Extra-Large") {
   	this.price += 3
   }
 }
 
 Pizza.prototype.changePriceToppings = function() {
-  this.price += (this.pizzaToppings.length * .75)
+  this.price += (this.pizzaToppings.length * .75);
 }
-
 
 //User Interface Logic
 $(document).ready(function() {
   $("form#pizzaMaker").submit(function() {
     event.preventDefault();
     var price = 15;
-    var size = $("select#sizeSelector").val();
+    var size = $("input:radio[name=optionsRadios]:checked").val();
     var pizzaToppings = [];
       $("input:checkbox[name=pizzaToppings]:checked").each(function(){
         var topping = $(this).val();
@@ -34,6 +33,11 @@ $(document).ready(function() {
     var newPizza = new Pizza(price, size, pizzaToppings);
     newPizza.changePriceSize();
     newPizza.changePriceToppings();
-    console.log(newPizza);
-});
+
+
+    $("#pizzaResult").fadeIn();
+    $("span.size").html("<h4>" + newPizza.size + "</h4>");
+    $("span.topping").html("<h4>" + newPizza.pizzaToppings + "</h4>");
+    $("span.price").html("<h4> $" + newPizza.price.toFixed(2) + "</h4>");
+  });
 });
