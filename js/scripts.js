665df579585e46925f5cf44a0ed696a7d49623dc
Gloria Friesen <gloriafriesen@gmail.com>
2017-03-03 09:50:14 -0800

    add changePrice prototype to change the prize of pizza based on size

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
 //Business Logic
-function Pizza (price, size, toppings) {
+function Pizza(price, size, toppings) {
   this.price = 15;
   this.size = pizzaSize;
   this.toppings = pizzaToppings;
 }
 
-// Pizza.prototype.addToppings = function() {
-//   pizzaToppings.push("mushrooms");
-// }
+Pizza.prototype.changePrice = function() {
+  if (this.size === "small") {
+    this.price -= 3
+  } else if (this.size === "large") {
+  	this.price += 3
+  }
+}
 
-
+var pizzaSize = "small";
+var pizzaToppings = ["pepperoni"];
+var newPizza = new Pizza(pizzaSize, pizzaToppings);
+newPizza.changePrice();
+console.log(newPizza);
 
 //User Interface Logic
-$(document).ready(function() {
-  var pizzaSize = $("input#pizzaSize").val();
-  var pizzaToppings = $("input#pizzaSize").val();
-  var newPizza = new Pizza();
-});
+// $(document).ready(function() {
+//   var pizzaSize = $("input#pizzaSize").val();
+//   var pizzaToppings = $("input#pizzaSize").val();
+//   var newPizza = new Pizza();
+// });
