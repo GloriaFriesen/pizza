d8034bf77607092d4275712e231b2a7294622016
Gloria Friesen <gloriafriesen@gmail.com>
2017-03-03 11:53:16 -0800

    call changePriceSize and changePriceToppings in user interface logic and added more size and toppings options in html

--- js/scripts.js
+++ js/scripts.js
@@ -5,16 +5,18 @@ function Pizza (price, size, pizzaToppings) {
   this.pizzaToppings = pizzaToppings;
 }
 
-Pizza.prototype.changePrice = function() {
+Pizza.prototype.changePriceSize = function() {
   if (this.size === "small") {
-    this.price -= 3
+    this.price -= 2
   } else if (this.size === "large") {
+  	this.price += 2
+  } else if (this.size === "extra-large") {
   	this.price += 3
   }
 }
 
 Pizza.prototype.changePriceToppings = function() {
-	this.price += (this.pizzaToppings.length * .5)
+  this.price += (this.pizzaToppings.length * .75)
 }
 
 
@@ -23,17 +25,15 @@ $(document).ready(function() {
   $("form#pizzaMaker").submit(function() {
     event.preventDefault();
     var price = 15;
-
     var size = $("select#sizeSelector").val();
-
     var pizzaToppings = [];
-    $("input:checkbox[name=pizzaToppings]:checked").each(function(){
-      var topping = $(this).val();
-      pizzaToppings.push(topping);
-    });
-
+      $("input:checkbox[name=pizzaToppings]:checked").each(function(){
+        var topping = $(this).val();
+        pizzaToppings.push(topping);
+      });
     var newPizza = new Pizza(price, size, pizzaToppings);
-
+    newPizza.changePriceSize();
+    newPizza.changePriceToppings();
     console.log(newPizza);
 });
 });
